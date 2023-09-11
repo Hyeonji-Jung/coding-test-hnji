56aa909afb1a42c896cb483c88df33633f2de7d4
HNJI <67726356+Hyeonji-Jung@users.noreply.github.com>
2023-09-11 16:12:31 +0900

    union-find 알고리즘을 이용한 풀이 추가

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,3 +1,4 @@
+# kruskal 알고리즘을 이용한 풀이
 def solution(n, costs):
     answer = 0
     costs.sort(key=lambda x : x[2])
@@ -10,4 +11,29 @@ def solution(n, costs):
                 answer += cost[2]
                 link.update([cost[0], cost[1]])
                 break
+    return answer
+
+# union-find를 이용한 풀이
+def solution(n, costs):
+    
+    def find(n, parent):
+        if parent[n] != n:
+            parent[n] = find(parent[n], parent)
+        return parent[n]
+    
+    def union(a, b, parent):
+        a = find(a, parent)
+        b = find(b, parent)
+        if a > b:
+            parent[a] = b
+        else:
+            parent[b] = a
+
+    answer = 0
+    costs.sort(key=lambda x : x[2])
+    parent = [i for i in range(n)]
+    for v1, v2, cost in costs:
+        if find(v1, parent) != find(v2, parent):
+            answer += cost
+            union(v1, v2, parent)
     return answer
