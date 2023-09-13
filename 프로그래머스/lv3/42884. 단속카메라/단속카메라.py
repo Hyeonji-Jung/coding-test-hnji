aae3f70ed7f6e5329c24b099845cb7c3e1654bab
HNJI <67726356+Hyeonji-Jung@users.noreply.github.com>
2023-09-13 15:22:47 +0900

    진출점을 기준으로 정렬한 풀이 추가

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -1,3 +1,4 @@
+# 진입점 기준으로 정렬한 풀이
 def solution(routes):
     routes.sort(key=lambda x:x[0])
     answer = 1
@@ -7,4 +8,15 @@ def solution(routes):
             answer += 1
             max_val = j
         max_val = min(max_val, j)
+    return answer
+
+# 진출점 기준으로 정렬한 풀이
+def solution(routes):
+    routes.sort(key=lambda x:x[1])
+    answer = 0
+    last_camera = -30000
+    for route in routes:
+        if last_camera < route[0]:
+            answer += 1
+            last_camera = route[1]
     return answer
