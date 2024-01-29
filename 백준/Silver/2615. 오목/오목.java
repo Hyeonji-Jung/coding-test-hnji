b187acb006a45d0c5038c45ebad9385b14ffb0c6
HNJI <67726356+Hyeonji-Jung@users.noreply.github.com>
2024-01-29 16:46:42 +0900

    [Silver I] Title: 오목, Time: 76 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.java
+++ 백준/Silver/2615. 오목/오목.java
@@ -2,30 +2,40 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * @author 정현지
+ * @date 240129
+ * @link https://www.acmicpc.net/problem/2615
+ * @keyword_solution 배열, 완전탐색
+ * @input 0~2로 구성된 19x19 2차원 배열, 띄어쓰기로 구분
+ * @output 0~2 사이의 자연수, 그 다음 줄에는 어느 한 팀이 이겼을 경우 가장 처음 발견된 바둑알의 위치
+ * @time_complex O(N^2)
+ * @perf 140ms
+ */
 
 public class Main {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static StringBuilder sb = new StringBuilder();
 	private static StringTokenizer tokens;
-	
+
 	private static int len = 19;
 	private static int winColor = 0;
-	
+
 	private static int[][] omok = new int[len][len];
-	
-	private static int[] dx = new int[]{1, 0, 1, -1};
-	private static int[] dy = new int[]{0, 1, 1, 1};
+
+	private static int[] dx = new int[] { 1, 0, 1, -1 };
+	private static int[] dy = new int[] { 0, 1, 1, 1 };
 
 	public static void main(String[] args) throws Exception {
-		
+
 		for (int i = 0; i < len; i++) {
 			tokens = new StringTokenizer(br.readLine().trim());
 			for (int j = 0; j < len; j++) {
 				omok[i][j] = Integer.parseInt(tokens.nextToken());
 			}
 		}
-		
-		loop : for (int i = 0; i < len; i++) {
+
+		loop: for (int i = 0; i < len; i++) {
 			for (int j = 0; j < len; j++) {
 				if (omok[i][j] != 0) {
 					for (int k = 0; k < 4; k++) {
@@ -33,7 +43,7 @@ public class Main {
 						int ry = j - dy[k];
 						if (rx >= 0 && rx < len && ry >= 0 && ry < len && omok[rx][ry] == omok[i][j])
 							continue;
-						
+
 						int count = 1;
 						int nx = i + dx[k];
 						int ny = j + dy[k];
@@ -46,7 +56,7 @@ public class Main {
 						if (count == 5) {
 							winColor = omok[i][j] == 1 ? 1 : 2;
 							sb.append(winColor).append("\n");
-							sb.append(i+1).append(" ").append(j+1);
+							sb.append(i + 1).append(" ").append(j + 1);
 							break loop;
 						}
 					}
