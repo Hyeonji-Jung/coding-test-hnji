124ce2531419d091249662ff5c1a5b2a5f95aa89
HNJI <67726356+Hyeonji-Jung@users.noreply.github.com>
2024-02-02 15:07:57 +0900

    [Gold IV] Title: N-Queen, Time: 5668 ms, Memory: 12028 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -8,26 +8,26 @@ public class Main {
 
 	private static boolean chk(int row, int[] board) {
 		for (int i = 0; i < row; i++)
-			if (board[i] == board[row] || row - i == Math.abs(board[row] - board[i]))
+			if (board[row] == board[i] || row - i == Math.abs(board[row] - board[i]))
 				return false;
 		return true;
 	}
 
-	private static void nQueen(int queens, int row, int[] board) {
-		if (queens == N) {
+	private static void nQueen(int row, int[] board) {
+		if (row == N) {
 			ways++;
 			return;
 		}
 		for (int i = 0; i < N; i++) {
 			board[row] = i;
 			if (chk(row, board))
-				nQueen(queens + 1, row + 1, board);
+				nQueen(row + 1, board);
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
 		N = Integer.parseInt(br.readLine());
-		nQueen(0, 0, new int[N]);
+		nQueen(0, new int[N]);
 		System.out.println(ways);
 	}
 }
