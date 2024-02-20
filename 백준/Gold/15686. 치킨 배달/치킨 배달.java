9eae3885db529ed86a9235b96074595b65dadda1
HNJI <67726356+Hyeonji-Jung@users.noreply.github.com>
2024-02-20 12:18:51 +0900

    [Gold V] Title: 치킨 배달, Time: 128 ms, Memory: 12488 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,57 +1,47 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
-class Pair {
-	int x;
-	int y; 
-	
-	public Pair(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-}
-
 public class Main {
-	
+
+	static class Pos {
+		int x;
+		int y;
+
+		Pos(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		static int getDist(Pos p1, Pos p2) {
+			return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
+		}
+	}
+
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static StringTokenizer tokens;
-	
-	private static int N, M;
-	private static int[][] city;
-	
-	private static List<Pair> houses = new ArrayList<>();
-	private static List<Pair> chickens = new ArrayList<>();
-	private static boolean[] alive;
-	private static int minDist = Integer.MAX_VALUE;
-	
-	private static int getDist(Pair house, Pair chicken) {
-		return Math.abs(house.x - chicken.x) + Math.abs(house.y - chicken.y);
-	}
-	
-	private static int getChickenDist() {
-		int dist = 0;
-		for (Pair house : houses) {
-			int currMinDist = Integer.MAX_VALUE;
-			for (int i = 0; i < chickens.size(); i++)
-				if (alive[i])
-					currMinDist = Math.min(currMinDist, getDist(house, chickens.get(i)));
-			dist += currMinDist;
-		}
-		return dist;
-	}
-	
-	private static void dfs(int start, int depth) {
+
+	private static int N, M, houseCnt, chickenCnt, minDist = Integer.MAX_VALUE;
+	private static ArrayList<Pos> houses = new ArrayList<>(), chickens = new ArrayList<>();
+	private static int[][] dists;
+	private static int[] selected;
+
+	private static void dfs(int depth, int start) {
 		if (depth == M) {
-			minDist = Math.min(minDist, getChickenDist());
+			int cityDist = 0;
+			for (int i = 0; i < houseCnt; i++) {
+				int currDist = Integer.MAX_VALUE;
+				for (int s : selected)
+					currDist = Math.min(currDist, dists[s][i]);
+				cityDist += currDist;
+			}
+			minDist = Math.min(minDist, cityDist);
 			return;
 		}
-		for (int i = start; i < chickens.size(); i++) {
-			alive[i] = true;
-			dfs(i + 1, depth + 1);
-			alive[i] = false;
+		for (int i = start; i < chickenCnt; i++) {
+			selected[depth] = i;
+			dfs(depth + 1, i + 1);
 		}
 	}
 
@@ -59,19 +49,26 @@ public class Main {
 		tokens = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(tokens.nextToken());
 		M = Integer.parseInt(tokens.nextToken());
-		city = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			tokens = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
-				city[i][j] = Integer.parseInt(tokens.nextToken());
-				if (city[i][j] == 1)
-					houses.add(new Pair(i, j));
-				else if (city[i][j] == 2)
-					chickens.add(new Pair(i, j));
+				switch (Integer.parseInt(tokens.nextToken())) {
+				case 1:
+					houses.add(new Pos(i, j));
+					houseCnt++;
+					break;
+				case 2:
+					chickens.add(new Pos(i, j));
+					chickenCnt++;
+					break;
+				}
 			}
 		}
-		alive = new boolean[chickens.size()];
-		
+		dists = new int[chickenCnt][houseCnt];
+		for (int i = 0; i < chickenCnt; i++)
+			for (int j = 0; j < houseCnt; j++)
+				dists[i][j] = Pos.getDist(chickens.get(i), houses.get(j));
+		selected = new int[M];
 		dfs(0, 0);
 		System.out.println(minDist);
 	}
