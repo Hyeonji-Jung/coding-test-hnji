321b9c16ccc91df0b013071cd9d3ce57c2e10bc2
HNJI <67726356+Hyeonji-Jung@users.noreply.github.com>
2024-03-05 13:34:37 +0900

    [Gold V] Title: 조 짜기, Time: 148 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Gold/2229. 조 짜기/조 짜기.java
+++ 백준/Gold/2229. 조 짜기/조 짜기.java
@@ -1,31 +1,37 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static StringTokenizer tokens;
-
-    private static int N;
-    private static int[] students, dp;
-
-    public static void main(String[] args) throws Exception {
+    public static int memo[]; 
+    public static int N;
+    public static int[] arr;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
         N = Integer.parseInt(br.readLine());
-        tokens = new StringTokenizer(br.readLine());
-        students = new int[N + 1];
-        for (int i = 1; i <= N; i++)
-            students[i] = Integer.parseInt(tokens.nextToken());
-        dp = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
+        arr = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        memo = new int[N+1];
+        for(int i=1; i<=N; i++) {
             int max = Integer.MIN_VALUE;
             int min = Integer.MAX_VALUE;
-            for (int j = i; j >= 1; j--) {
-                max = Math.max(max, students[j]);
-                min = Math.min(min, students[j]);
-                dp[i] = Math.max(dp[i], max - min + dp[j - 1]);
+            for(int j=i; j>0; j--) {
+                if(max<arr[j]) max = arr[j];
+                if(min>arr[j]) min = arr[j];
+                memo[i] = Math.max(memo[i], max-min + memo[j-1]); 
+                //기존 i값
+                //1~j-1까지 고려해 최대 잘짜여진 팀에 j부터i까지 묶어서 팀짰을때
             }
         }
-        System.out.println(dp[N]);
+//        System.out.println(Arrays.toString(memo));
+        System.out.println(memo[N]);
+        
     }
 }
