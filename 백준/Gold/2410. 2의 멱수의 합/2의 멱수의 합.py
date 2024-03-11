3f315fdc18ea81792b92ce55c9882fe2626bb5a8
HNJI <67726356+Hyeonji-Jung@users.noreply.github.com>
2024-03-11 11:17:12 +0900

    [Gold V] Title: 2의 멱수의 합, Time: 132 ms, Memory: 117056 KB -BaekjoonHub

---
+++ 백준/Gold/2410. 2의 멱수의 합/2의 멱수의 합.py
@@ -0,0 +1,10 @@
+N = int(input())
+dp = [0 for _ in range(1_000_001)]
+dp[1] = 1
+dp[2] = 2
+for i in range(3, N + 1):
+    if (i % 2 == 1):
+        dp[i] = dp[i - 1]
+    else:
+        dp[i] = (dp[i - 1] + dp[i // 2]) % 1_000_000_000
+print(dp[N])
