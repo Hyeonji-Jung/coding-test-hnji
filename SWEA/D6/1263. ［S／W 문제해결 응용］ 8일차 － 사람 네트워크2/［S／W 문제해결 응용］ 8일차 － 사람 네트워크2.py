e4276e6a2b3a1e6f78cf3150925cb44e498d0b2b
HNJI <67726356+Hyeonji-Jung@users.noreply.github.com>
2024-03-29 11:54:57 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 13,409 ms, Memory: 218,252 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.py
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.py
@@ -1,20 +1,55 @@
-T = int(input())
-for t in range(1, T + 1):
-    tmp = list(map(int, input().split()))
-    N = tmp[0]
-    net = [tmp[i:i+N] for i in range(1, N * N, N)]
-    for i in range(N):
-        for j in range(N):
-            if i != j and net[i][j] == 0:
-                net[i][j] = int(1e9)
-    for k in range(N):
-        for i in range(N):
-            for j in range(N):
-                net[i][j] = min(net[i][j], net[i][k] + net[k][j])
-    ans = int(1e9)
-    for i in range(N):
-        curr = 0
-        for j in range(N):
-            curr += net[i][j]
-        ans = min(ans, curr)
-    print(f"#{t} {ans}")
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    static final int INF = (int) 1e9;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+
+            int[][] net = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    net[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (i != j && net[i][j] == 0) {
+                        net[i][j] = INF;
+                    }
+                }
+            }
+
+            for (int k = 0; k < N; k++) {
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < N; j++) {
+                        net[i][j] = Math.min(net[i][j], net[i][k] + net[k][j]);
+                    }
+                }
+            }
+
+            int ans = INF;
+            for (int i = 0; i < N; i++) {
+                int curr = 0;
+                for (int j = 0; j < N; j++) {
+                    curr += net[i][j];
+                }
+                ans = Math.min(ans, curr);
+            }
+
+            sb.append("#").append(t).append(" ").append(ans).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
