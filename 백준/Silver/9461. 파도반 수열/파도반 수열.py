e41952715c636a391493e7b1e8b7396b8d7d6318
HNJI <67726356+Hyeonji-Jung@users.noreply.github.com>
2024-04-02 14:37:50 +0900

    [Silver III] Title: 파도반 수열, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -4,14 +4,9 @@ input = sys.stdin.readline
 t = int(input())
 ans = [0] * 101
 ans[1] = ans[2] = ans[3] = 1
-
-def padoban(n):
-    if ans[n] != 0:
-        return
-    for i in range(4, n + 1):
-        ans[i] = ans[i - 2] + ans[i - 3]
+for i in range(4, 101):
+    ans[i] = ans[i-3] + ans[i-2]
 
 for _ in range(t):
     n = int(input())
-    padoban(n)
     print(ans[n])
